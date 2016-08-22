c98db30b5c0cee087f3e9e5247afb1cf3dead265
Codati <codatitiss@gmail.com>
2016-08-22 17:13:41 +0200

    add chefdeprojet

--- class/controller/login.php
+++ class/controller/login.php
@@ -15,14 +15,24 @@ class Login {
   }
 
   function connect() {
-    $user = \Model\Utilisateur::getUser($_POST['password'], $_POST['login']);
+    $user = false;
+    switch ($_POST['type']) {
+      case "utilisateur":
+        $user = \Model\Utilisateur::getUser($_POST['password'], $_POST['login']);
+
+
+        break;
+      case "chefdeprojet":
+        $user = \Model\ChefDeProjet::getChefDeProjet($_POST['password'], $_POST['login']);
+        break;
+    }
 
     if ($user) {
-      var_dump($user);
-      $_SESSION['user'] = $user;
+      $_SESSION[$_POST['type']] = $user;
       header('Location: dashboard');
     } else {
-      header('Location: ');
+      $_SESSION['lastPost'] = $_POST;
+      header('Location: ./');
     }
   }
 
---
+++ class/model/chefdeprojet.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Utilisateur
+ *
+ * @author codati
+ */
+
+namespace Model;
+class ChefDeProjet {
+
+  private $id, $password, $login;
+
+  static public function getChefDeProjet($pass, $login) {
+
+    $query = \Bdd::getInstence()->prepare('SELECT * FROM `ChefDeProjet` WHERE `login` = :login');
+    $query->bindParam('login', $login);
+
+    $query->execute();
+    
+    if ($query->rowCount() == 1) {
+      $user = $query->fetchObject(__CLASS__);
+
+      if (password_verify($pass, $user->password)) {
+        return $user;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+}
