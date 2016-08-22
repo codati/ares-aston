76ad4e81891eb39cd55fd960223e41fa3d46c06f
Codati <codatitiss@gmail.com>
2016-08-22 14:02:01 +0200

    add folder view

--- public/index.php
+++ public/index.php
@@ -4,25 +4,6 @@ session_start();
 require_once '../bootstrap.php';
 
 $bdd = Bdd::getInstence();
-?>
 
-<!doctype html>
-<html lang="fr">
-	<head>
-		<meta charset="utf-8">
-		<title>Login</title>
-		<link rel="stylesheet" href="style.css">
-		<script src="script.js"></script>
-	</head>
-	<body>
-		<div>
-			<form action="login.php" method="post">
-					<input type="text" name="login">
-					<input type="password" name="password">
 
-					<button type="submit">Se connecter</button>
-			</form>
-		</div>
-	</body>
-</html>
-
+require PATH_VIEW.'login.php';
---
+++ view/login.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <div>
+      <form action="login.php" method="post">
+        <input type="text" name="login">
+        <input type="password" name="password">
+        <button type="submit">Se connecter</button>
+      </form>
+    </div>
+  </body>
+</html>
+
