7e6be55d59ba551012563c2417911b985068bf02
Codati <codatitiss@gmail.com>
2016-08-22 14:16:28 +0200

    creat folder css

--- view/login.php
+++ view/login.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Login</title>
-    <link rel="stylesheet" href="../public/styles.css">
-    <script src="script.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+   <!-- <script src="script.js"></script>-->
   </head>
   <body>
     <h1>Plateforme ARES</h1>
