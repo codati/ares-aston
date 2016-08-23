16e52c7e02d8e0a16440b61f54c74b6e19d2d757
Cyrial42 <martinbiervliet@orange.fr>
2016-08-23 12:00:45 +0200

    Création formulaire

--- view/addTache.php
+++ view/addTache.php
@@ -2,7 +2,7 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
-    <title>AddTache</title>
+    <title>Ajouter une demande</title>
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
@@ -15,18 +15,26 @@
             <img alt="Ares" class="logo-a" src="images/logo.jpg">
           </a>
         </div>
-        <h1 class="main-title">Plateforme ARES</h1>
+        <h1 class="main-title">Création d'une demande</h1>
       </div>
     </nav>
     
-    <div class="login-container">
-      <form action="login" method="post">
+    <div class="form-container">
+      <form action="addTache" method="post">
         <input class="form-control input" type="text" name="titre" placeholder="Titre">
-        <textarea  placeholder="Description">
-          
-        </textarea>
-        <p>Role utilisateur</p>
-        <button class="login-inline btn btn-success" type="submit">save</button>
+        <p>Description détaillé de la demande : </p>
+        <textarea  placeholder="Description" cols="87" rows="8" maxlength="88">
+        </textarea><br>
+        <p>Date d'échéance : </p>
+        <input type="date" name="echeance" class="echeance"><br>
+        <p>Temps prévisionel (en minute) : </p>
+        <input type="number" name="tmp-prevision" min="0" step="5"><br>
+        <p>Assigné à :</p>
+        <select>
+          <option value="nomprenom">Nom Prénom</option>
+          <option value="nomprenom">Nom Prénom</option> 
+        </select><br>
+        <button class="login-inline btn btn-success" type="submit">Créer la demande</button>
       </form>
     </div>
     <script src="js/jquery-3.1.0.js"></script>
