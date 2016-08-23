a1515b3c64a51a66d294697a46a2c0d079b8d48f
Codati <codatitiss@gmail.com>
2016-08-23 15:39:53 +0200

    chrono detail page

--- view/details.php
+++ view/details.php
@@ -6,8 +6,11 @@
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <script src="js/jquery-3.1.0.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.js"></script>
+
   </head>
-  <body>
+  <body ng-app="ares" ng-controller="details as details">
     <nav class="navbar navbar-default">
       <div class="container-fluid">
         <div class="navbar-header">
@@ -22,19 +25,19 @@
 
     <div class="detail-container col-md-5">
       <h2>Titre :</h2>
-      <p class="title-details"><?= $tache->getTitre();?></p> 
+      <p class="title-details"><?= $tache->getTitre(); ?></p> 
       <hr>
       <h2>Temps estimé :</h2>
-      <p><?= $tache->getTmpRealisation();?></p>
+      <p> {{ time }}</p>
       <hr>
       <h2>Description :</h2>
-      <p class="description-text"><?= $tache->getDescription();?>.</p>
+      <p class="description-text"><?= $tache->getDescription(); ?>.</p>
     </div>
 
     <div class="chrono-container col-md-6">
       <h2>Temps écoulé :</h2>
       <div class="container-btn">
-        <p id="heures">00</p><p class="separator">:</p><p id="minutes">00</p><p class="separator-s">:</p><p id="secondes">00</p><br>
+        <span ng-show="timeChrono.total < 0"> - </span> <p id="heures">{{ timeChrono.h }}</p><p class="separator">:</p><p id="minutes">{{ timeChrono.m }}</p><p class="separator-s">:</p><p id="secondes">{{ timeChrono.s }}</p><br>
         <button class="btn btn-chrono btn-primary" id="btn-play"><i class="fa fa-play-circle-o"></i></button>
         <button class="btn btn-chrono btn-danger" id="btn-pause"><i class="fa fa-pause-circle-o"></i></button>
         <button class="btn btn-success btn-chrono" id="btn-stop"><i class="fa fa-stop-circle-o"></i></button>
@@ -53,5 +56,58 @@
 
     <script src="js/jquery-3.1.0.js"></script>
     <script src="js/scripts.js"></script>
+
+
+    <script type="text/javascript">
+
+      angular.module('ares', [])
+              .controller('details', function ($scope, $interval) {
+
+                console.log('test');
+                $scope.timeChrono = [];
+                $scope.timeChrono.total = -5500;
+
+
+                var h = Math.floor(<?= $tache->getTmpRealisation(); ?> / 60);
+
+                var m = <?= $tache->getTmpRealisation(); ?> % 60;
+                h = h < 10 ? '0' + h : h;
+                m = m < 10 ? '0' + m : m;
+                $scope.time = h + ':' + m;
+
+                $interval(chrone, 1000);
+
+                chrone();
+
+
+                function chrone() {
+
+
+
+                  $scope.timeChrono.s = Math.abs($scope.timeChrono.total % 60);
+                  $scope.timeChrono.s = $scope.timeChrono.s < 10 ? 0 + '' + $scope.timeChrono.s : $scope.timeChrono.s;
+
+                  if ($scope.timeChrono.total < 0)
+                  {
+                    $scope.timeChrono.m = Math.abs(Math.ceil($scope.timeChrono.total / 60) % 60);
+                    $scope.timeChrono.h = Math.abs(Math.ceil($scope.timeChrono.total / 3600));
+
+                  } else {
+                    $scope.timeChrono.m = Math.floor($scope.timeChrono.total / 60) % 60;
+                    $scope.timeChrono.m = $scope.timeChrono.m > 0 ? 0 + '' + $scope.timeChrono.m : $scope.timeChrono.m;
+                    $scope.timeChrono.h = Math.floor($scope.timeChrono.total / 3600);
+
+                  }
+                  $scope.timeChrono.m = $scope.timeChrono.m < 10 ? 0 + '' + $scope.timeChrono.m : $scope.timeChrono.m;
+                  $scope.timeChrono.h = $scope.timeChrono.h < 10 ? 0 + '' + $scope.timeChrono.h : $scope.timeChrono.h;
+
+                  $scope.timeChrono.total--;
+
+                }
+              });
+
+
+    </script>
+
   </body>
 </html>
