35529bf6e8beacea1c0f4aaca075542fcfc6f53f
Codati <codatitiss@gmail.com>
2016-08-24 15:55:46 +0200

    send utilisateur to view userboard

--- class/controller/userboard.php
+++ class/controller/userboard.php
@@ -10,27 +10,8 @@ namespace Controller;
 class Userboard {
 
   public function index() {
-    $data['messages'] = $_SESSION['messages'];
-    \Tools::renderView('userboard',$data);
-    
-    
-     $_SESSION['messages'] = [];
-//    
-//    $tache = new \Model\Tache();
-//    $tache->setTitre('test');
-//    $tache->setEtat('enCours');
-//    $tache->setTmpRealisation(10);
-//    $tache->setEcheance(time());
-//    $tache->setDescription('test');
-//    $tache->setId_utilisateur(1);
-//    $tache->save();
-//
-//
-//
-//    $tache = \Model\Tache::getbyId('1');
-//    $tache->setTitre('LOL');
-//    $tache->setTitre('the Description');
-//    $tache->save();
+    $data['utilisateurs'] = \Model\Utilisateur::getAll();
+    \Tools::renderView('userboard', $data);
   }
 
   public function viewPlanning() {
