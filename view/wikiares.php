0d193c8340fbebedd2f43f5c68479d697312a0d2
Codati <codatitiss@gmail.com>
2016-08-25 16:49:51 +0200

    fix link icone

--- view/wikiares.php
+++ view/wikiares.php
@@ -12,7 +12,7 @@
   	<nav class="navbar navbar-default">
       <div class="container-fluid">
         <div class="navbar-header">
-          <a class="navbar-brand" href="#">
+          <a class="navbar-brand" href="/">
             <img alt="Ares" class="logo-a" src="images/logo.jpg">
           </a>
         </div>
